4f5acb895d38e1332d9ea7f175b3fd0147d9a5a9
askwoos <114606516+askwoos@users.noreply.github.com>
2022-10-05 15:16:01 +0900

    1005

--- java_basic/src/java_basic/scanner.java
+++ java_basic/src/java_basic/scanner.java
@@ -5,11 +5,32 @@ public class scanner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      
-		Scanner scan = new Scanner(System.in);
-		
-
+		visitIsland("Corfu");
+		 
+	}//main class
 	
-	}
+	public static void visitIsland(String island){
+        switch(island) {
+         case "Corfu":
+              System.out.println("User wants to visit Corfu");
+              break;
+         case "Crete":
+              System.out.println("User wants to visit Crete");
+              break;
+         case "Santorini":
+              System.out.println("User wants to visit Santorini");
+              break;
+         case "Mykonos":
+              System.out.println("User wants to visit Mykonos");
+              break;
+        default:
+              System.out.println("Unknown Island");
+              break;
+        }// switch
+   }// close visit method
+	 
+	
+	
 
 }
 
