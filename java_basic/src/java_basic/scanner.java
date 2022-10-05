360830b64d98722fb4e7644c59d59c7f68a4d65d
askwoos <114606516+askwoos@users.noreply.github.com>
2022-10-05 10:16:55 +0900

    22

---
+++ java_basic/src/java_basic/scanner.java
@@ -0,0 +1,15 @@
+package java_basic;
+import java.util.Scanner;
+public class scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		Scanner scan = new Scanner(System.in);
+		
+
+	
+	}
+
+}
+
